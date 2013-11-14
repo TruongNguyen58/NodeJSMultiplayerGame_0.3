b276f8496cbad093a099bd07121ac4eb1f1fc043
Truong Nguyen <nguyenhuutruong58@gmail.com>
2013-11-14 12:00:57 +0700

    minor changes

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var allowCrossDomain = function(req, res, next) {
 
 app.configure(function() {
 	app.use(allowCrossDomain);
-	app.set('port', 443);
+	app.set('port', 3002);
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
 	app.use(express.favicon());
